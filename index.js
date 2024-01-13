886a439197c3307f73662f746e1a028e31864afe
stephen scheiman <sbscheiman@gmail.com>
2024-01-12 19:13:40 -0800

    Initial logo creation, circles only

--- index.js
+++ index.js
@@ -1,77 +1,42 @@
 const inquirer = require("inquirer");
-const { Shapes, Square } = require("./lib/shapes");
+const fs = require("fs");
+const { Shape, Circle } = require("./lib/shapes");
 
-const initialShapeQ = inquirer
-.prompt([
+const questions = inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "initials",
+      message: "Please enter three letters for your SVG Logo",
+    },
+    {
+      type: "input",
+      name: "txtColor",
+      message: "Please enter the text color for your SVG Logo",
+    },
     {
       type: "list",
-      name: "Shape",
-      message: "Please pick a shape for your SVG Logo",
-      choices: ["Square","Triangle","Circle"],
+      name: "shape",
+      message: "Please enter the shape for your SVG Logo",
+      choices: ["Circle", "Square", "Triangle"],
     },
-])
-.then((answer) => {
-    this.shape = answer['Shape'];
-    if(this.shape === "Square"){
-        console.log(this.shape);
-        squareTmp();
-    } else if (this.shape === "Circle"){
-        console.log(this.shape);
-        circleTmp();
-    } else {
-        console.log(this.shape);
-        triangleTmp();
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Please enter the color of the shape for your SVG Logo",
+    },
+  ])
+  .then((answers) => {
+    if (answers["shape"] === "Circle") {
+      const circle = new Circle(
+        answers["initials"],
+        answers["txtColor"],
+        answers["shape"],
+        answers["shapeColor"]
+      );
+      const tmpLogo = circle.render();
+      fs.writeFile(`${answers["initials"]}.svg`, tmpLogo, (err) =>
+        err ? console.log(err) : console.log("Success!")
+      );
     }
-});
-
-
-function squareTmp() { inquirer
-    .prompt([
-    {
-        type: "input",
-        name: "width",
-        message: "Please pick a width for your square",
-    },
-    {
-        type: "input",
-        name: "height",
-        message: "Please pick a height for your square",
-    },
-    ])
-    .then((answer) => {
-        const width = answer["width"];
-        const height = answer["height"];
-        console.log(width);
-        console.log(height);
-        const square = new Square("Square", width, height);
-        square.render();
-    });
-}
-
-function circleTmp() { inquirer
-    .prompt([
-    {
-        type: "input",
-        name: "radius",
-        message: "Please pick a radius for your circle",
-    },
-    ])
-    .then((answer) => {
-        const radius = answer["radius"];
-        console.log(radius);
-    });
-}
-
-// function triangleTmp() { inquirer
-//     .prompt([
-//     {
-//         type: "input",
-//         name: "radius",
-//         message: "Please pick a radius for your circle",
-//     },
-//     ])
-//     .then((answer) => {
-//         const radius = answer["radius"];
-//         console.log(radius);
-//     });
-// }
+  });
